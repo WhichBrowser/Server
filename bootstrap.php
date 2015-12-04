daca90f7cce740363cee47c4fc7c0d412475cc6a
Niels Leenheer <niels@leenheer.nl>
2015-12-04 07:46:39 +0100

    Set up this repository for further development

--- bootstrap.php
+++ bootstrap.php
@@ -9,8 +9,3 @@
 	elseif (strpos(__FILE__, DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR) !== false) {
 		include_once __DIR__ . '/../../autoload.php';
 	}
-
-	// Development environment
-	else {
-		include_once __DIR__ . '/../parser/bootstrap.php';
-	}
