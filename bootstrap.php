05c11f5f5666bc1baff8a601e1c5c2805638b2ad
Niels Leenheer <niels@leenheer.nl>
2015-12-09 21:53:47 +0100

    Add local bootstrap for the local development directory of the parser

--- bootstrap.php
+++ bootstrap.php
@@ -9,3 +9,8 @@
 	elseif (strpos(__FILE__, DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR) !== false) {
 		include_once __DIR__ . '/../../autoload.php';
 	}
+
+    // Local development directory
+    elseif (file_exists(__DIR__ . '/../Parser/bootstrap.php')) {
+        include_once __DIR__ . '/../Parser/bootstrap.php';
+    }
